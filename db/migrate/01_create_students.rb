121fa7a032b313658bd57a62a5df35df2e97a84f
Elana Ryznar <eryzna@gmail.com>
2019-01-26 00:57:26 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -2,9 +2,6 @@ class CreateStudents < ActiveRecord::Migration[5.1]
   def change
     create_table :students do |t|
       t.string :name
-      t.string :grade
-      t.string :birthdate
-      #t.string :hometown
     end
   end
 end
